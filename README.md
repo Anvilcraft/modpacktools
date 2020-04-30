ba5e81267e7a797d5f33e7ec5670927c0d2f6917
LordMZTE <lord@mzte.de>
2020-04-30 21:13:17 +0200

    add readme and some packages

---
+++ README.md
@@ -0,0 +1,2 @@
+# modpacktools
+This is a WIP java app to help modpack makers by creating Twitch exports, mod lists and downloading the mods
---
+++ src/main/java/ley/anvil/modpacktools/util/Config.java
@@ -0,0 +1,7 @@
+package ley.anvil.modpacktools.util;
+
+public class Config {
+	Config() {
+		//TODO this should copy the modpacktoolsconfig.json file from resources to the jar file's dir it it doesnt exist and load it otherwise
+	}
+}
