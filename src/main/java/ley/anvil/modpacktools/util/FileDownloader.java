e83776994fc5c5d525fcbd0f7f47f9a83c365a30
LordMZTE <lord@mzte.de>
2020-07-18 16:41:28 +0200

    remove unused fields

--- src/main/java/ley/anvil/modpacktools/util/FileDownloader.java
+++ src/main/java/ley/anvil/modpacktools/util/FileDownloader.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -47,8 +46,6 @@ public class FileDownloader {
         Map<URL, File> files;
         Consumer<DownloadFileTask.Return> callback;
         int tries;
-        long threadTimeout;
-        TimeUnit threadTimeoutUnit;
         int httpTimeout;
         ExistingFileBehaviour existingFileBehaviour;
 
