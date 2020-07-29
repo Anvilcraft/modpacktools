8863e703bc360fd5ff6bb28f0b68259db25551ed
LordMZTE <lord@mzte.de>
2020-07-29 23:52:22 +0200

    add toString to MissingConfigValueException

--- src/main/kotlin/ley/anvil/modpacktools/util/config/MissingConfigValueException.kt
+++ src/main/kotlin/ley/anvil/modpacktools/util/config/MissingConfigValueException.kt
@@ -6,4 +6,7 @@ package ley.anvil.modpacktools.util.config
  * @param missingValue the config value that is missing
  * @param message an optional message to be displayed
  */
-data class MissingConfigValueException(val missingValue: String, override val message: String? = null) : IllegalStateException(message)
+data class MissingConfigValueException(val missingValue: String, override val message: String? = null) : IllegalStateException(message) {
+    //overridden to get better looking stack trace
+    override fun toString(): String = "MissingConfigValueException: Value $missingValue not found: ${message ?: ""}"
+}
